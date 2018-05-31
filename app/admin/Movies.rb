6f79c4a94f42f9927a8f497049b53209579c803f
rabinpoudyal1995 <rabinpoudyal1995@gmail.com>
2018-05-31 18:08:18 +0545

    Fix active Admin

---
+++ app/admin/Movies.rb
@@ -0,0 +1,2 @@
+ActiveAdmin.register Movie do 
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -41,14 +41,16 @@ require 'csv'
 
 # puts "Took approx #{end_time - start_time} to complete."
 
-csv_text = File.read(Rails.root.join('lib', 'seed_data', 'ratings.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
-	r = Rating.new
-	r.user_id = row['userId']
-	r.movie_id = row['movieId']
-	r.rating = row['rating']
-	r.rated_at = row['timestamp']
-	r.save
-	puts "#{r.user_id} rated #{r.movie_id} movie successfully"
-end
+# csv_text = File.read(Rails.root.join('lib', 'seed_data', 'ratings.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+# 	r = Rating.new
+# 	r.user_id = row['userId']
+# 	r.movie_id = row['movieId']
+# 	r.rating = row['rating']
+# 	r.rated_at = row['timestamp']
+# 	r.save
+# 	puts "#{r.user_id} rated #{r.movie_id} movie successfully"
+# end
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
